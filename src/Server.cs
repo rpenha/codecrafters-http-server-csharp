4cc544d71477366f92ce1498a4bf4eba5f815ecd
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-06-02 18:00:08 -0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,30 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+
+var cts = new CancellationTokenSource();
+var ct = cts.Token;
 
 var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 
+var notFound = "HTTP/1.1 404 Not Found\r\n\r\n"u8.ToArray();
+var ok = "HTTP/1.1 200 OK\r\n\r\n"u8.ToArray();
+
+const int bufferSize = 1024;
+
 using var socket  = server.AcceptSocket(); // wait for client
-var response = "HTTP/1.1 200 OK\r\n\r\n"u8.ToArray();
-await socket.SendAsync(response.ToArray());
+HashSet<string> urls = ["/", "index.html"];
+var buffer = new byte[bufferSize];
+
+await socket.ReceiveAsync(buffer, ct);
+
+using var reader = new StringReader(Encoding.UTF8.GetString(buffer));
+
+var line = await reader.ReadLineAsync(ct);
+
+var url = line!.Split(' ')[1];
+
+var response = urls.Contains(url) ? ok : notFound;
+
+await socket.SendAsync(response);
