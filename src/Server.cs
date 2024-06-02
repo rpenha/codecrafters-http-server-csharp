47cfd89073745eefb0a79b7f764576054a4a66b3
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-06-02 18:14:36 -0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -13,8 +13,8 @@ var ok = "HTTP/1.1 200 OK\r\n\r\n"u8.ToArray();
 
 const int bufferSize = 1024;
 
-using var socket  = server.AcceptSocket(); // wait for client
-HashSet<string> urls = ["/", "index.html"];
+using var socket = server.AcceptSocket(); // wait for client
+
 var buffer = new byte[bufferSize];
 
 await socket.ReceiveAsync(buffer, ct);
@@ -24,7 +24,13 @@ using var reader = new StringReader(Encoding.UTF8.GetString(buffer));
 var line = await reader.ReadLineAsync(ct);
 
 var url = line!.Split(' ')[1];
+var urlFragments = url.Split('/', StringSplitOptions.RemoveEmptyEntries);
 
-var response = urls.Contains(url) ? ok : notFound;
+var response = urlFragments switch
+{
+    [] => ok,
+    ["echo", var msg] => Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {msg.Length}\r\n\r\n{msg}"),
+    _ => notFound
+};
 
 await socket.SendAsync(response);
