554ca6b76b4b5a8a3cc53bcae3abe3226b9af694
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-07-25 16:26:30 -0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -42,84 +42,76 @@ async Task HandleRequest(TcpClient socket, CancellationToken cancellationToken)
 {
     var notFound = "HTTP/1.1 404 Not Found\r\n\r\n"u8.ToArray();
     var ok = "HTTP/1.1 200 OK\r\n\r\n"u8.ToArray();
+    var stream = socket.GetStream();
+    var buffer = new byte[socket.Available];
+    _ = await stream.ReadAsync(buffer, cancellationToken);
+    using var reader = new StringReader(Encoding.UTF8.GetString(buffer.ToArray()));
+    var line = await reader.ReadLineAsync(cancellationToken);
+    var parts = line!.Split(' ');
+    var verb = parts[0];
+    var url = parts[1];
+    var urlFragments = url.Split('/', StringSplitOptions.RemoveEmptyEntries);
 
-    try
+    // Headers
+    var headers = new Dictionary<string, string>();
+    var sb = new StringBuilder();
+    var isBody = false;
+
+    while (await reader.ReadLineAsync(cancellationToken) is { } value)
     {
-        var stream = socket.GetStream();
-        var buffer = new byte[socket.Available];
-        _ = await stream.ReadAsync(buffer, cancellationToken);
-        using var reader = new StringReader(Encoding.UTF8.GetString(buffer.ToArray()));
-        var line = await reader.ReadLineAsync(cancellationToken);
-        var parts = line!.Split(' ');
-        var verb = parts[0];
-        var url = parts[1];
-        var urlFragments = url.Split('/', StringSplitOptions.RemoveEmptyEntries);
-
-        // Headers
-        var headers = new Dictionary<string, string>();
-        var sb = new StringBuilder();
-        var isBody = false;
-
-        while (await reader.ReadLineAsync(cancellationToken) is { } value)
+        if (string.IsNullOrWhiteSpace(value) && !isBody)
         {
-            if (string.IsNullOrWhiteSpace(value) && !isBody)
-            {
-                isBody = true;
-            }
+            isBody = true;
+        }
             
-            if (!isBody)
-            {
-                var kv = value.Split(": ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-                headers.Add(kv[0], kv[1]);
-            }
-            else
-            {
-                sb.Append(value);
-            }
-        }
-
-        var response = (verb, urlFragments) switch
+        if (!isBody)
         {
-            ("GET", []) => ok,
-            ("GET", ["echo", var msg]) => Encoding.UTF8.GetBytes(
-                $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {msg.Length}\r\n\r\n{msg}"
-            ),
-            ("GET", ["user-agent"]) => Encoding.UTF8.GetBytes(
-                $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {headers["User-Agent"].Length}\r\n\r\n{headers["User-Agent"]}"
-            ),
-            ("GET", ["files", var filename]) => await GetFileAsync(filename),
-            ("POST", ["files", var filename]) => await PostFileAsync(filename, sb.ToString()),
-            _ => notFound
-        };
-
-        async Task<ArraySegment<byte>> PostFileAsync(string filename, string body)
-        {
-            var path = Path.Combine(Environment.CurrentDirectory, filename);
-            await File.WriteAllTextAsync(path, body, cancellationToken);
-            return Encoding.UTF8.GetBytes($"HTTP/1.1 201 Created\r\nContent-Length: {body.Length}\r\n\r\n{body}");
+            var kv = value.Split(": ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            headers.Add(kv[0], kv[1]);
         }
-
-        async Task<ArraySegment<byte>> GetFileAsync(string filename)
+        else
         {
-            var directory = Environment.CurrentDirectory;
-            var path = Path.Combine(directory, filename);
-
-            if (!File.Exists(path)) return notFound;
-
-            var data = await File.ReadAllBytesAsync(path, cancellationToken);
-
-            return new ArraySegment<byte>([
-                ..Encoding.UTF8.GetBytes(
-                    $"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {data.Length}\r\n\r\n"),
-                ..data
-            ]);
+            sb.Append(value);
         }
-
-        await using var writer = new StreamWriter(stream);
-        await writer.WriteAsync(Encoding.UTF8.GetChars(response.ToArray()));
     }
-    finally
+
+    var response = (verb, urlFragments) switch
     {
-        socket.Dispose();
+        ("GET", []) => ok,
+        ("GET", ["echo", var msg]) => Encoding.UTF8.GetBytes(
+            $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {msg.Length}\r\n\r\n{msg}"
+        ),
+        ("GET", ["user-agent"]) => Encoding.UTF8.GetBytes(
+            $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {headers["User-Agent"].Length}\r\n\r\n{headers["User-Agent"]}"
+        ),
+        ("GET", ["files", var filename]) => await GetFileAsync(filename),
+        ("POST", ["files", var filename]) => await PostFileAsync(filename, sb.ToString()),
+        _ => notFound
+    };
+
+    async Task<ArraySegment<byte>> PostFileAsync(string filename, string body)
+    {
+        var path = Path.Combine(Environment.CurrentDirectory, filename);
+        await File.WriteAllTextAsync(path, body, cancellationToken);
+        return Encoding.UTF8.GetBytes($"HTTP/1.1 201 Created\r\nContent-Length: {body.Length}\r\n\r\n{body}");
     }
+
+    async Task<ArraySegment<byte>> GetFileAsync(string filename)
+    {
+        var directory = Environment.CurrentDirectory;
+        var path = Path.Combine(directory, filename);
+
+        if (!File.Exists(path)) return notFound;
+
+        var data = await File.ReadAllBytesAsync(path, cancellationToken);
+
+        return new ArraySegment<byte>([
+            ..Encoding.UTF8.GetBytes(
+                $"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {data.Length}\r\n\r\n"),
+            ..data
+        ]);
+    }
+
+    await using var writer = new StreamWriter(stream);
+    await writer.WriteAsync(Encoding.UTF8.GetChars(response.ToArray()));
 }
